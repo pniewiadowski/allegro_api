db7c8666905a194f7a62d6ed4bbefa224cd253c4
Przemysław Niewiadowski <niewiadowski@gmail.com>
2017-08-16 06:30:29 +0200

    soappy

--- allegro_api.py
+++ allegro_api.py
@@ -1,3 +1,6 @@
+from soappy import wsdl
 
 allegro_api_key = open('api_key.txt').readline()
+sandbox_web_api='https://webapi.allegro.pl.webapisandbox.pl/service.php?wsdl'
+prod_web_api = 'https://webapi.allegro.pl/service.php?wsdl'
 
