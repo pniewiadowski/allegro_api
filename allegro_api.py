919410da92f3959d68dc2040495b6b0fad33aa68
Przemysław Niewiadowski <niewiadowski@gmail.com>
2017-08-17 06:47:16 +0200

    switch to zeep to handle SOAP

--- allegro_api.py
+++ allegro_api.py
@@ -1,6 +1,9 @@
-from soappy import wsdl
+import zeep
 
 allegro_api_key = open('api_key.txt').readline()
 sandbox_web_api='https://webapi.allegro.pl.webapisandbox.pl/service.php?wsdl'
 prod_web_api = 'https://webapi.allegro.pl/service.php?wsdl'
+
+client = zeep.Client(wsdl=prod_web_api)
+print(client.service.doGetItemsList(allegro_api_key,1))
 
