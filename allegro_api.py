bd727f92ff96be7b57ee9712af5d3bf1725eae11
Przemysław Niewiadowski <niewiadowski@gmail.com>
2017-08-22 06:47:32 +0200

    filterOptionList

--- allegro_api.py
+++ allegro_api.py
@@ -1,9 +1,14 @@
 import zeep
 
 allegro_api_key = open('api_key.txt').readline()
-sandbox_web_api='https://webapi.allegro.pl.webapisandbox.pl/service.php?wsdl'
+sandbox_web_api = 'https://webapi.allegro.pl.webapisandbox.pl/service.php?wsdl'
 prod_web_api = 'https://webapi.allegro.pl/service.php?wsdl'
 
 client = zeep.Client(wsdl=prod_web_api)
-print(client.service.doGetItemsList(allegro_api_key,1))
+filterOptionsList = {'item':
+                         {'filterId': 'category',
+                          'filterValueId': 'householdHealth'
+                          }
+                     }
 
+print(client.service.doGetItemsList(allegro_api_key, 1, filterOptions=filterOptionsList))
